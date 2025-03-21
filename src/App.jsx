6e5b502907b814abf0b672024568e0d05c84f1ff
GuranaHemanth <pravallika3404@example.com>
2025-03-21 23:22:05 +0530

    Switch to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom'; // Changed to HashRouter
 import Login from './components/Auth/Login';
 import RoomList from './components/Rooms/RoomList';
 import VideoRoom from './components/Rooms/VideoRoom';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter> {/* Changed from BrowserRouter */}
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/rooms" element={<RoomList />} />
         <Route path="/room/:roomId" element={<VideoRoom />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
